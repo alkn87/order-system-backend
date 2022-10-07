af51f8a0939317738f26ddf8b891681e13506525
Alexander Knapp <alexander.knapp@objectbay.com>
2022-10-07 12:15:36 +0200

    initial commit

---
+++ ordersystem/src/main/kotlin/at/fhcampuswien/sde/ordersystem/OrdersystemApplication.kt
@@ -0,0 +1,11 @@
+package at.fhcampuswien.sde.ordersystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrdersystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrdersystemApplication>(*args)
+}
